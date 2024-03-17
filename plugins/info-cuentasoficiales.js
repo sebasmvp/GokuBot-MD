f17e5a723585438e365ef90af1a919015afaf938
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-16 21:47:24 -0500

    Actualizar info-cuentasoficiales.js

--- plugins/info-cuentasoficiales.js
+++ plugins/info-cuentasoficiales.js
@@ -1,16 +1,13 @@
 
 
-let media = './Menu2.jpg'
+let media = './Menu3.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    await conn.sendMessage(m.chat, { react: { text: '🧸', key: m.key } })
+    await conn.sendMessage(m.chat, { react: { text: '🌼', key: m.key } })
 let str = `𝖡𝗂𝖾𝗇𝗏𝖾𝗇𝖽𝗂𝗈 𝖠 𝖫𝖺𝗌 𝖢𝗎𝖾𝗇𝗍𝖺𝗌 𝖮𝖿𝗂𝖼𝗂𝖺𝗅𝖾𝗌 😻
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-🌹 *Bot Oficial:*
-Wa.me/573218138672
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ⚡️ *Propietario:*
-Wa.me/573012482694
+_Jostin_
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 🧸 *Github Del Bot:*
 *${md}*
@@ -23,11 +20,11 @@ Wa.me/573012482694
 🧸 *Canal De Actualización Del Bot:*
  *${channel}*\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+🌹 *Canal Informa:*
+*${channel2}*\n
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 📍 *Canal De Youtube:*
 ${yt}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-📍 *Facebook:*
-${fb}
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 handler.command = /^cuentas|cuentasoficiales$/i
