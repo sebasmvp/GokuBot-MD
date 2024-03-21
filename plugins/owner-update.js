be421deb318ecc43b0df0a7354d93f6c12563d62
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-20 20:40:07 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -41,24 +41,3 @@ handler.rowner = true
 
 export default handler
 
-
-//no quites los creditos
-
-/*import {execSync} from "child_process"
-let handler = async (m, {conn, text}) => {
-  await m.reply(`🐱 𝐄 𝐒 𝐏 𝐄 𝐑 𝐀 🚀`)
-  try {
-    if (global.conn.user.jid == conn.user.jid) {
-      let stdout = execSync("git pull" + (m.fromMe && text ? " " + text : ""))
-      await await await conn.reply(m.chat, stdout.toString(), m)
-    }
-  } catch {
-    var update = execSync("git remote set-url origin https://github.com/diegojadibot/SakuraBotLite-MD-.git && git pull")
-    await await await m.reply(update.toString())
-  }
-};
-handler.help = ["update"]
-handler.tags = ["owner"]
-handler.command = /^update|actualizar$/i
-handler.rowner = true
-export default handler*/
