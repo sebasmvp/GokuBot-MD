e9e98736ac971ebb1207aca08b612836392d7584
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-03-22 15:58:03 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,9 +4,9 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
        title: '𝐿𝑖𝑛𝑘 𝐺𝑟𝑜𝑢𝑝',
-      body: '𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯🍁🌱',    
+      body: 'GokuBot-MD🍁🌱',    
       previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
- sourceUrl: `https://atom.bio/sakuralite`}}});
+ sourceUrl: `https://atom.bio/gokubot-md`}}});
 };     
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
