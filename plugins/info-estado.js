a38c40177748f3b8a3e457eca2bbf4d32ecd61c1
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-03-31 02:09:27 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -5,38 +5,22 @@ const handler = async (m, {conn}) => {
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
-    const str = `> 𝙃𝙤𝙡𝙖 𝙪𝙨𝙪𝙖𝙧𝙞𝙤/𝙖: ${taguser}, 𝙗𝙞𝙚𝙣𝙫𝙚𝙣𝙞𝙙𝙤/𝙖 𝙖𝙡 𝙚𝙨𝙩𝙖𝙙𝙤 𝙙𝙚𝙡 𝙗𝙤𝙩.
-> •━━━━━━━━━━━━━━━━━•
-> ⓘ 𝙈𝙊𝘿𝙊 𝘿𝙀 𝘽𝙊𝙏:
-> ✦ Publico
-> •━━━━━━━━━━━━━━━━━•
-> ⓘ 𝙊𝙒𝙉𝙀𝙍𝙎 𝙊𝙁𝘾:
-> ✦ wa.me/5491168758497
-> ✦ wa.me/593939005387
-> •━━━━━━━━━━━━━━━━━•
-> ⓘ 𝘾𝙐𝙀𝙉𝙏𝘼𝙎:
-> ✦ ${gp1}
-> ✦ ${yt}
-> ✦ ${md}
-> •━━━━━━━━━━━━━━━━━•
-> ⓘ 𝙏𝙄𝙀𝙈𝙋𝙊:
-> ✦ ${uptime}
-> •━━━━━━━━━━━━━━━━━•
-> ⓘ 𝘾𝙊𝙉𝙉𝙀𝘾𝙏𝙄𝙊𝙉:
-> ✦ GokuBot-MD
-> •━━━━━━━━━━━━━━━━━•
-> ⓘ *_VERSIÓN_* :
-> ✦ ${vs}
-> •━━━━━━━━━━━━━━━━━•
-> ⓘ 𝙉𝘼𝙈𝙀 𝘽𝙊𝙏:
-> ✦ ${wm}
-> •━━━━━━━━━━━━━━━━━•`.trim();
+    await conn.sendMessage(m.chat, { react: { text: '🤠', key: m.key } })
+    const str = `┏✧ » ◇ « ✧ » ✦ « ✧ » ◇ « ✧
+┃⍣ *_GOKUBOT-MD_* ✨️🌻
+┃
+┃⍣👋🏻𝘏𝘰𝘭𝘢: ${taguser}
+┃
+┃⍣ *ꨄ︎ ⏳️ 𝘈𝘤𝘵𝘪𝘷𝘰:* ${uptime}
+┃⍣ *ꨄ︎ ✅ 𝘉𝘰𝘵 𝘜𝘴𝘰 Publico*
+┃⍣ *ꨄ︎ 🦁 𝘖𝘸𝘯𝘦𝘳: Jostin*
+┃⍣ *ꨄ︎ 🔗 𝘊𝘶𝘦𝘯𝘵𝘢𝘴 𝘖𝘧𝘤:* https://atom.bio/gokubotmd
+╰✧ » ◇ « ✧ » ✦ « ✧ » ◇ « ✧`.trim();
     if (m.isGroup) {
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://youtube.com/@user-bw5wl4ye8r?si=qN8bP4ZdKm3yXwbx`, "sourceUrl": `https://youtube.com/@user-bw5wl4ye8r?si=qN8bP4ZdKm3yXwbx`}}}, {quoted: m});
+      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://atom.bio/gokubotmd`, "sourceUrl": `https://atom.bio/gokubotmd`}}}, {quoted: m});
     } else {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://youtube.com/@user-bw5wl4ye8r?si=qN8bP4ZdKm3yXwbx`, "sourceUrl": `https://youtube.com/@user-bw5wl4ye8r?si=qN8bP4ZdKm3yXwbx`}}}, {quoted: fkontak2});
+      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://atom.bio/gokubotmd`, "sourceUrl": `https://atom.bio/gokubotmd`}}}, {quoted: fkontak2});
     }
   } catch {
   }
@@ -50,5 +34,5 @@ function clockString(ms) {
   const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [`` + d, ' *Día(s)* ', `• ` + h, ' *Hora(s)* ', `• ` + m, ' *Minuto(s)* ', `• ` + s, ' *Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
-  }
+  return [`\n│ *=> 💥 ` + d, ' Día(s)* ', `\n│ *=> 💫 ` + h, ' Hora(s)* ', `\n│ *=> 💠 ` + m, ' Minuto(s)* ', `\n│ *=> ♦ ` + s, ' Segundo(s)* '].map((v) => v.toString().padStart(2, 0)).join('');
+        }
