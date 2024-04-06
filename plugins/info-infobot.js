e64cb2044c61a23b7528c9fa141cf2de2e2cf871
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-06 12:29:06 -0500

    Actualizar info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -33,7 +33,7 @@ let texto = `*🚀 Velocidad*
 *💻 Servidor*
 • *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
 
-conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: 'GOKU - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: 'Bot', body: 'GOKU - BOT - MD', thumbnailUrl: 'https://telegra.ph/file/6cbf9148b572711e9b000.jpg', sourceUrl: 'https://chat.whatsapp.com/G9MpDVmJtbiIpItEZpg70s', mediaType: 1, renderLargerThumbnail: true }}})
 
 }
 handler.help = ['ping']
