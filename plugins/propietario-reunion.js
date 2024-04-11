75a7041e277752e4b4d340107296aa1f05f2ee18
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-11 10:36:36 -0500

    Actualizar propietario-reunion.js

--- plugins/propietario-reunion.js
+++ plugins/propietario-reunion.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn, command }) => {
 
 let text = `🐒 𝖬𝗂 𝗉𝗋𝗈𝗉𝗂𝖾𝗍𝖺𝗋𝗂𝗈 @${m.sender.split`@`[0]} 𝗁𝖺 𝗌𝗈𝗅𝗂𝖼𝗂𝗍𝖺𝖽𝗈 𝗌𝗎 𝗉𝗋𝖾𝗌𝖾𝗇𝖼𝗂𝖺 𝖾𝗇 𝖾𝗅 𝗌𝗍𝖺𝖿𝖿, 𝗉𝗈𝗋 𝖿𝖺𝗏𝗈𝗋 𝖽𝖾 𝖽𝗂𝗋𝗂𝗀𝗂𝗋𝗌𝖾 𝗅𝗈 𝖺𝗇𝗍𝖾𝗌 𝗉𝗈𝗌𝗂𝖻𝗅𝖾 𝖺𝗅 𝗀𝗋𝗎𝗉𝗈 𝖽𝖾 𝗌𝗍𝖺𝖿𝖿 🚩`
 m.reply('🚩 *ENVIANDO*')
-for (let [jid] of global.cowner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
+for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
                         conn.sendPayment(data.jid, '999999999', text, m)
