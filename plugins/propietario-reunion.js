b7e738bebf1c42a08a9b0d990c621091e323d935
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-11 10:36:21 -0500

    Actualizar propietario-reunion.js

--- plugins/propietario-reunion.js
+++ plugins/propietario-reunion.js
@@ -1,25 +1,14 @@
-let handler = async(m, { conn, command, text }) => {
+let handler = async(m, { conn, command }) => {
 
-  let users = m.sender.split`@`[0]
-  if ( users == 593939005387 || users == 50558124470) try {
-  if (!text) return m.reply(` ¡𝗖𝘂𝗮𝗹 𝗲𝘀 𝗲𝗹 𝗠𝗼𝘁𝗶𝘃𝗼 𝗽𝗮𝗿𝗮 𝗹𝗮 𝗿𝗲𝘂𝗻𝗶𝗼𝗻 𝗱𝗲𝗹 𝘀𝘁𝗮𝗳𝗳!`)
-    if (text.length < 10) return m.reply(`¡𝗠𝗼𝘁𝗶𝘃𝗼 𝗺𝘂𝘆 𝗰𝗼𝗿𝘁𝗼 , 𝗺𝗶𝗻𝗶𝗺𝗼 10 𝗰𝗮𝗿𝗮𝗰𝘁𝗲𝗿𝗲𝘀!`)
-
-    let texto = `✨ @${m.sender.split`@`[0]} 𝐀 𝐒𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐋𝐚 𝐏𝐫𝐞𝐬𝐞𝐧𝐜𝐢𝐚 𝐃𝐞 𝐋𝐨𝐬 𝐎𝐰𝐧𝐞𝐫 𝐏𝐨𝐫 𝐌𝐨𝐭𝐢𝐯𝐨: 
-¡${text}
-Staff GokuBot - Jostin 🌹`
-m.reply('✨ Se Le Ah Enviado El Mensaje Ah Los Propietarios Del Bot')
-for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
+let text = `🐒 𝖬𝗂 𝗉𝗋𝗈𝗉𝗂𝖾𝗍𝖺𝗋𝗂𝗈 @${m.sender.split`@`[0]} 𝗁𝖺 𝗌𝗈𝗅𝗂𝖼𝗂𝗍𝖺𝖽𝗈 𝗌𝗎 𝗉𝗋𝖾𝗌𝖾𝗇𝖼𝗂𝖺 𝖾𝗇 𝖾𝗅 𝗌𝗍𝖺𝖿𝖿, 𝗉𝗈𝗋 𝖿𝖺𝗏𝗈𝗋 𝖽𝖾 𝖽𝗂𝗋𝗂𝗀𝗂𝗋𝗌𝖾 𝗅𝗈 𝖺𝗇𝗍𝖾𝗌 𝗉𝗈𝗌𝗂𝖻𝗅𝖾 𝖺𝗅 𝗀𝗋𝗎𝗉𝗈 𝖽𝖾 𝗌𝗍𝖺𝖿𝖿 🚩`
+m.reply('🚩 *ENVIANDO*')
+for (let [jid] of global.cowner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
-                        conn.sendPayment(data.jid, '99', texto, m)}
-  } catch (e) { await conn.reply(m.chat, '𝗖𝗼𝗺𝗮𝗻𝗱𝗼 𝗻𝗼 𝗳𝘂𝗻𝗰𝗶𝗼𝗻𝗮𝗹', m)
-               console.log(e)
-               } else {
-    await m.reply('¡𝗧𝘂 𝗻𝗼 𝘁𝗶𝗲𝗻𝗲𝘀 𝗽𝗲𝗿𝗺𝗶𝘀𝗼 𝗽𝗮𝗿𝗮 𝘂𝘀𝗮𝗿 𝗲𝘀𝘁𝗲 𝗰𝗼𝗺𝗮𝗻𝗱𝗼!')}
+                        conn.sendPayment(data.jid, '999999999', text, m)
 
                            // conn.reply(data.jid, text, m, { mentions: [m.sender] })
-
+                    }
 
 }
 handler.tags = ['owner']
