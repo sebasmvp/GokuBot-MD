feae0022413cd810daca381810ff32d47f413bca
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-11 21:45:15 -0500

    Actualizar emogis.js

--- plugins/emogis.js
+++ plugins/emogis.js
@@ -1,11 +1,13 @@
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
+      const sms = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
 const pp = './storage/logos/Menu2.jpg'
 
 let love = `> Mensaje: ${text} 🐒\n\n*El puto de ${name} busca marido. ⭐️*`
 
-conn.sendMessage(m.chat, {text: love, mentions: [m.sender]}, {quoted: m})
+conn.sendMessage(m.chat, {text: love, mentions: [m.sender]}, {quoted: sms})
 
 //conn.reply(m.chat, `*🚩 Ocurrió un fallo*`)
 
