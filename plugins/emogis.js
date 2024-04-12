7a403c5a977eb4e9b9da0d18db78e607098fabb9
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-11 21:30:55 -0500

    Actualizar emogis.js

--- plugins/emogis.js
+++ plugins/emogis.js
@@ -3,7 +3,9 @@ let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.m
 let name = conn.getName(who)
 const pp = './storage/logos/Menu2.jpg'
 
-let love = `${text}`
+let love = `${text}
+
+*Tu Puto ${name} Invoca*`
 
 conn.sendMessage(m.chat, {text: love, mentions: [m.sender]}, {quoted: m})
 
@@ -12,7 +14,7 @@ conn.sendMessage(m.chat, {text: love, mentions: [m.sender]}, {quoted: m})
 }
 handler.help = ['love']
 handler.tags = ['juegos']
-handler.command = /^(decir|hablar|emogi|emogis)$/i
+handler.command = /^(decir|enviar|emogi|emogis)$/i
 
 //handler.register = true
 
