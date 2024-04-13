59b0399e5ec05aa8ce2287432d737092da5595f6
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-13 12:10:14 -0500

    Actualizar fix-owner-esperando_mensajes.js

--- plugins/fix-owner-esperando_mensajes.js
+++ plugins/fix-owner-esperando_mensajes.js
@@ -1,17 +1,17 @@
-/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
-
+/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino 
+*/
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync } from 'fs';
 import path from 'path';
-
+            const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
 const handler = async (m, { conn, usedPrefix }) => {
   if (global.conn.user.jid !== conn.user.jid) {
-    return conn.sendMessage(m.chat, {text: '*🧸 Tienes Que Utilizarlo En El Numero Del Bot Oficial.*'}, {quoted: m});
+    return conn.sendMessage(m.chat, {text: '*🧸 Tienes Que Utilizarlo En El Numero Del Bot Oficial.*'}, {quoted: fkontak2});
   }
-  await conn.sendMessage(m.chat, {text: '*🧩 Iɴɪᴄɪᴀɴᴅᴏ Eʟ Pʀᴏᴄᴇsᴏ Dᴇ Eʟɪᴍɪɴᴀᴄɪᴏ́ɴ Dᴇ Tᴏᴅᴏs Lᴏs Aʀᴄʜɪᴠᴏs Dᴇ Sᴇssɪᴏɴ, Exᴇᴄᴛᴏ Eʟ Aʀᴄʜɪᴠᴏ creds.json...*'}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: '*🧩 Iɴɪᴄɪᴀɴᴅᴏ Eʟ Pʀᴏᴄᴇsᴏ Dᴇ Eʟɪᴍɪɴᴀᴄɪᴏ́ɴ Dᴇ Tᴏᴅᴏs Lᴏs Aʀᴄʜɪᴠᴏs Dᴇ Sᴇssɪᴏɴ, Exᴇᴄᴛᴏ Eʟ Aʀᴄʜɪᴠᴏ creds.json...*'}, {quoted: fkontak2});
   const sessionPath = './GokuBotSession/';
   try {
     if (!existsSync(sessionPath)) {
-      return await conn.sendMessage(m.chat, {text: '*📍 La Carpeta GokuBotSession No Existe O Está Vacia.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '*📍 La Carpeta GokuBotSession No Existe O Está Vacia.*'}, {quoted: fkontak2});
     }
     const files = await fs.readdir(sessionPath);
     let filesDeleted = 0;
@@ -22,15 +22,15 @@ const handler = async (m, { conn, usedPrefix }) => {
       }
     }
     if (filesDeleted === 0) {
-      await conn.sendMessage(m.chat, {text: '*📍 No Se Encontro El Archivo Para Eliminar La Carpeta GokuBotSession.*'}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: '*📍 No Se Encontro El Archivo Para Eliminar La Carpeta GokuBotSession.*'}, {quoted: fkontak2});
     } else {
-      await conn.sendMessage(m.chat, {text: `*🧸 Se Eliminaron ${filesDeleted} Archivos De Sesion, Excepto El Archivo creds.json.*`}, {quoted: m});
+      await conn.sendMessage(m.chat, {text: `*🧸 Se Eliminaron ${filesDeleted} Archivos De Sesion, Excepto El Archivo creds.json.*`}, {quoted: fkontak2});
     }
   } catch (err) {
     console.error('Error al leer la carpeta o los archivos de sesión:', err);
-    await conn.sendMessage(m.chat, {text: '*🚀 Ocurrió Un Error Al Eliminar Los Archivos De Sesion.*'}, {quoted: m});
+    await conn.sendMessage(m.chat, {text: '*🚀 Ocurrió Un Error Al Eliminar Los Archivos De Sesion.*'}, {quoted: fkontak2});
   }
-  await conn.sendMessage(m.chat, {text: `*🧸 ¡Hola! Ahora me ves?*\n\n*🧩 Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*📍 Ejemplo:*\n${usedPrefix}wm\n${usedPrefix}wm\n${usedPrefix}wm`}, {quoted: m});
+  await conn.sendMessage(m.chat, {text: `*🧸 ¡Hola! Ahora me ves?*\n\n*🧩 Si el Bot no le responde a sus comandos por favor haga un pequeño spam*\n\n*📍 Ejemplo:*\n${usedPrefix}wm\n${usedPrefix}wm\n${usedPrefix}wm`}, {quoted: fkontak2});
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
