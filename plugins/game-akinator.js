78058a153f315eb6a6c443a0e88cab6ada1f190c
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-04-24 17:57:44 -0500

    Actualizar game-akinator.js

--- plugins/game-akinator.js
+++ plugins/game-akinator.js
@@ -4,16 +4,16 @@ const handler = async (m, {conn, usedPrefix, command, text}) => {
   if (m.isGroup) return;
   const aki = global.db.data.users[m.sender].akinator;
   if (text == 'end') {
-    if (!aki.sesi) return m.reply('*[❗] 𝙰𝙲𝚃𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴 𝙽𝙾 𝙴𝚂𝚃𝙰𝚂 𝙴𝙽 𝚄𝙽𝙰 𝚂𝙴𝚂𝙸𝙾𝙽 (𝙿𝙰𝚁𝚃𝙸𝙳𝙰) 𝙳𝙴 𝙰𝙺𝙸𝙽𝙰𝚃𝙾𝚁*');
+    if (!aki.sesi) return m.reply('> ⓘ 𝙉𝙤 𝙚𝙨𝙩𝙖𝙨 𝙚𝙣 𝙪𝙣𝙖 𝙥𝙖𝙧𝙩𝙞𝙙𝙖 𝙙𝙚 𝘼𝙠𝙞𝙣𝙖𝙩𝙤𝙧');
     aki.sesi = false;
     aki.soal = null;
-    m.reply('*[❗] 𝚂𝙴 𝙴𝙻𝙸𝙼𝙸𝙽𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾 𝙻𝙰 𝚂𝙴𝚂𝙸𝙾𝙽 (𝙿𝙰𝚁𝚃𝙸𝙳𝙰) 𝙳𝙴 𝙰𝙺𝙸𝙽𝙰𝚃𝙾𝚁*');
+    m.reply('> ⓘ Se ha eliminado correctamente la session de Akinator.');
   } else {
-    if (aki.sesi) return conn.reply(m.chat, '*[❗] 𝚃𝙾𝙳𝙰𝚅𝙸𝙰 𝙴𝚂𝚃𝙰𝚂 𝙴𝙽 𝚄𝙽𝙰 𝚂𝙴𝚂𝙸𝙾𝙽 (𝙿𝙰𝚁𝚃𝙸𝙳𝙰) 𝙳𝙴 𝙰𝙺𝙸𝙽𝙰𝚃𝙾𝚁*', aki.soal);
+    if (aki.sesi) return conn.reply(m.chat, '> ⓘ Aun estas en una session de Akinator.', aki.soal);
     try {
       const res = await fetch(`https://api.lolhuman.xyz/api/akinator/start?apikey=${lolkeysapi}`);
       const anu = await res.json();
-      if (anu.status !== 200) throw '*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*';
+      if (anu.status !== 200) throw '> ⓘ Ocurrio un error inesperado, por favor vuelva mas tarde.';
       const {server, frontaddr, session, signature, question, progression, step} = anu.result;
       aki.sesi = true;
       aki.server = server;
@@ -24,21 +24,21 @@ const handler = async (m, {conn, usedPrefix, command, text}) => {
       aki.progression = progression;
       aki.step = step;
       const resultes2 = await translate(question, {to: 'es', autoCorrect: false});
-      let txt = `🎮 *𝐀𝐊𝐈𝐍𝐀𝐓𝐎𝐑* 🎮\n\n*𝙹𝚄𝙶𝙰𝙳𝙾𝚁: @${m.sender.split('@')[0]}*\n*𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰: ${resultes2.text}*\n\n`;
+      let txt = `> 💠 𝐀𝐊𝐈𝐍𝐀𝐓𝐎𝐑 💠\n\n> 𝙅𝙪𝙜𝙖𝙙𝙤𝙧: @${m.sender.split('@')[0]}\n> 𝙋𝙧𝙚𝙜𝙪𝙣𝙩𝙖: *${resultes2.text}*\n\n`;
       txt += '*0 - Sí*\n';
       txt += '*1 - No*\n';
       txt += '*2 - No sé*\n';
       txt += '*3 - Probablemente sí*\n';
       txt += '*4 - Probablemente no*\n\n';
-      txt += `*𝚄𝚂𝙰 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} end 𝙿𝙰𝚁𝙰 𝚂𝙰𝙻𝙸𝚁 𝙳𝙴 𝙻𝙰 𝚂𝙴𝚂𝙸𝙾𝙽 (𝙿𝙰𝚁𝚃𝙸𝙳𝙰) 𝙳𝙴 𝙰𝙺𝙸𝙽𝙰𝚃𝙾𝚁*`;
+      txt += `> 💠 𝙋𝙪𝙚𝙙𝙚 𝙪𝙨𝙖𝙧 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤: ${usedPrefix + command} end 𝙥𝙖𝙧𝙖 𝙨𝙖𝙡𝙞𝙧 𝙚𝙣 𝙚𝙡 𝙟𝙪𝙚𝙜𝙤 𝙙𝙚 𝘼𝙠𝙞𝙣𝙖𝙩𝙤𝙧.`;
       const soal = await conn.sendMessage(m.chat, {text: txt, mentions: [m.sender]}, {quoted: m});
       aki.soal = soal;
     } catch {
-      m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*');
+      m.reply('> ⓘ Error, el juego de \`Akinator\` ha caido, vuelva mas tarde.');
     }
   }
 };
 handler.menu = ['akinator'];
 handler.tags = ['game'];
-handler.command = /^(akinator)$/i;
+handler.command = /^(akinatorssss)$/i;
 export default handler;
