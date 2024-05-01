8e565dddaef43fb463488e1e480ed6e4422faa74
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-01 11:56:37 -0500

    Actualizar config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -362,7 +362,7 @@ break;
 ╰━━━⊰ ${vs} ⊱━━━━╯`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: `${wm}`,
-body: '', previewType: 0, thumbnail: imagen4, sourceUrl: [gp2, gp1, md].getRandom()}}})} 
+body: saludo, previewType: 0, thumbnail: imagen4, sourceUrl: [gp2, gp1, md].getRandom()}}})} 
 
 handler.help = ['en', 'dis'].map((v) => v + 'able <option>');
 handler.tags = ['group', 'owner'];
