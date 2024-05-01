1289b2336d74152534dae212c9ebb6a835fea995
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-30 21:27:11 -0500

    Actualizar allestilo.js

--- plugins/allestilo.js
+++ plugins/allestilo.js
@@ -1,1 +1,5 @@
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Goku Bot - MD\nJostin', orderTitle: 'Bang', thumbnail: imagen6, sellerJid: '0@s.whatsapp.net'}}}
+//global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Goku Bot - MD\nJostin', orderTitle: 'Bang', thumbnail: imagen6, sellerJid: '0@s.whatsapp.net'}}}
+
+global.gMenu = [imagen1, imagen6, imagen4].getRandom()
+
+const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Super Bot WhatsApp', orderTitle: 'Bang', thumbnail: gMenu, sellerJid: '0@s.whatsapp.net' }}}
