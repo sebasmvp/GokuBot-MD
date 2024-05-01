eedefa68a87cf18f24a09e52b166d943a6f8f3d4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-30 21:59:27 -0500

    Actualizar bot-saludo.js

--- plugins/bot-saludo.js
+++ plugins/bot-saludo.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
 
-          m.reply(`_${saludo}_ *_Humano!_*`, estilo);
+          m.reply(`_${saludo}_ *_Humano!_*`);
             });
 }
 handler.help = ['info']
