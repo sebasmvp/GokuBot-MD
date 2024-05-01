a0533f3cca8287078ce5cd1739db25c7057afd90
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-30 19:43:35 -0500

    Actualizar info-cuentasoficiales.js

--- plugins/info-cuentasoficiales.js
+++ plugins/info-cuentasoficiales.js
@@ -1,4 +1,4 @@
-/*let media = './Menu3.png'
+let media = './Menu3.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `𝖡𝗂𝖾𝗇𝗏𝖾𝗇𝖽𝗂𝗈 𝖠 𝖫𝖺𝗌 𝖢𝗎𝖾𝗇𝗍𝖺𝗌 𝖮𝖿𝗂𝖼𝗂𝖺𝗅𝖾𝗌 De GokuBot-MD 😻
@@ -30,4 +30,4 @@ await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 handler.command = /^cuentas|cuentasoficiales$/i
 handler.exp = 35
 //handler.register = true
-export default handler*/
+export default handler
