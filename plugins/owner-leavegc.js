0947bd8c92a89a55850b66f8297195fdeee96efe
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-30 21:11:23 -0500

    Actualizar owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,16 +1,12 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-let chat = global.db.data.chats[m.chat]
-chat.welcome = false
-await conn.reply(id, `😻 GokuBot-MD *Abandona El Grupo Fué Genial Estar Aqui Con Ustedes,Gracias Por Quererme 👋*`) 
-await conn.groupLeave(id)
-try {  
-chat.welcome = true
-} catch (e) {
-await m.reply(`${fg}`) 
-return console.log(e)
-}}
-handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
+let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
+await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*${saludo}*', mentions: [m.sender] }, { quoted: estilo })
+await conn.groupLeave(id)}
+handler.help = ['salir']
+handler.tags = ['owner']
+handler.command = /^(salir|out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
 handler.rowner = true
+
 export default handler
