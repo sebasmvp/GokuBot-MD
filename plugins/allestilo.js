099a52f5c1de63ad00fda0858f1c6e10d3d4c666
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-30 21:38:24 -0500

    Actualizar allestilo.js

--- plugins/allestilo.js
+++ plugins/allestilo.js
@@ -1,5 +1,7 @@
 //global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Goku Bot - MD\nJostin', orderTitle: 'Bang', thumbnail: imagen6, sellerJid: '0@s.whatsapp.net'}}}
 
+global.foto = 'https://telegra.ph/file/ee6da72848e646e81a1a0.jpg'
+
 global.catalogo = [imagen1, imagen6, imagen4].getRandom()
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Goku Bot - MD\nCreated By ', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Goku Bot - MD\nCreated By ', orderTitle: 'Bang', thumbnail: global.foto, sellerJid: '0@s.whatsapp.net'}}}
