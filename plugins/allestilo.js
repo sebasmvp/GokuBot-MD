8dee8a0b52995e8e6c4192ad9438eea4860a35bc
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-04-30 21:31:15 -0500

    Actualizar allestilo.js

--- plugins/allestilo.js
+++ plugins/allestilo.js
@@ -2,4 +2,4 @@
 
 global.catalogo = [imagen1, imagen6, imagen4].getRandom()
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Goku Bot - MD\nCreated By ', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Goku Bot - MD\nCreated By ', orderTitle: 'Bang', thumbnail: imagen6, sellerJid: '0@s.whatsapp.net'}}}
